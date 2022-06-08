99b7e19acd09f8bfff0107c1fe3ad41c44f556b6
95Hamilton95 <hamilton.solis@catolica.edu.sv>
2022-06-07 22:55:24 -0600

    login cambio

--- SistemaInformaticoAzoc/Formularios/Sistema/FrmInicioSesion.cs
+++ SistemaInformaticoAzoc/Formularios/Sistema/FrmInicioSesion.cs
@@ -6,19 +6,25 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SistemaInformaticoAzoc.Servicios;
 using System.Windows.Forms;
 
 namespace SistemaInformaticoAzoc.Formularios.Sistema
 {
     public partial class FrmInicioSesion : Form
     {
+        private readonly UsuarioService usarioService;
         public FrmInicioSesion()
         {
-            InitializeComponent();
+            InitializeComponent(); 
+             usarioService = new UsuarioService();
+
         }
 
         private void BtnIniciarSesion_Click(object sender, EventArgs e)
         {
+            var nombre=TxtNombre.Text;
+            var clave=TxtClave.Text;
             DialogResult = DialogResult.OK;
         }
 
